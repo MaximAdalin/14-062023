b7e7908d3313eff255a9a1652eca789d7debc9f0
Maxim.Adalin <adalin.maks@mail.ru>
2023-06-18 22:07:37 +0600

    script for index.html

---
+++ script.js
@@ -0,0 +1,14 @@
+let sum, num1, num2, num3;
+
+num1 = 2;
+num2 = 3;
+num3 = 1;
+
+sum = num1 + num2 + num3;
+
+if (sum === 6) {
+    alert('Сумма трёх чисел равна 6')
+} else {
+    alert('false')
+}
+
